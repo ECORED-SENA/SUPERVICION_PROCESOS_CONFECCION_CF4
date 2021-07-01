e227e3871e5b04ce7d343bc72b420f7d54e9bf17
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2021-07-01 06:30:19 -0500

    Hasta tema 1

---
+++ src/views/Introduccion.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+.curso-main-container.introduccion
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  
+    .titulo-principal
+      .titulo-principal__numero
+        span
+          i.fas.fa-info
+      h1 Introducción
+    
+    p.mb-4 En la supervisión de procesos de confección, desde el que hacer es importante interpretar los patrones de las prendas según requerimientos de ficha técnica y orden de producción, para cumplir con este propósito se hace necesario el aprendizaje en el desarrollo de moldes, planos, y escalados en softwares especializados de patronaje, la industrialización y modernización de procesos exige cada vez más, el conocimiento básico y destreza en el manejo de programas digitales, en este caso se aprenderá el manejo básico de tres (3) de ellos, Audaces 360, OptiTex y Gerber para los procesos productivos según disponibilidad de la empresa.
+
+    .bloque1.bloque-texto-b.color-acento-contenido.p-4.mb-5
+      .bloque-texto-b__texto
+        figure
+          img(src='@/assets/curso/img01.png')          
+        
+
+    p El manejo un software permite optimizar tiempos en la elaboración de patrones en las diferentes líneas de producción. La inmersión en el mundo digital se hace cada vez más predominante; es por ello, que en este espacio se hará un reconocimiento de la interfaz y el manejo de diferentes tipos de herramientas sin embargo el conocimiento técnico sobre el desarrollo de patrones o moldes ya ha sido adquirido y se implementará la misma base teórica trabaja de manera manual, dentro de la cadena de producción uno de los pasos del patronaje corresponde a trazo y corte, en este espacio se hará un con texto general, bajo la terminología de producción industrial en trazo y corte, este tipo de vocabulario, herramientas y conocimiento hace parte de los conceptos en cultura general qué debe tener un patronista.
+
+</template>
+
+<script>
+export default {
+  name: 'Introduccion',
+  data: () => ({
+    // variables de vue
+  }),
+}
+</script>
+
+<style lang="sass" scoped>
+.introduccion
+</style>
---
+++ src/views/Tema1.vue
@@ -0,0 +1,360 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+
+    .titulo-principal
+      .titulo-principal__numero
+        span 1
+      h1 Audaces 360
+
+    .row.mb-5
+      .col-lg-7.d-flex.align-items-center
+        div
+          p.mb-5 Como ya se ha mencionado dentro del sector productivo de la moda, hay diferentes softwares que permiten optimización de todos los recursos que comprende una empresa dentro de los departamentos de producción, uno de ellos es el dedicado a la generación de modelos o diseños integrado generalmente por:
+          p Estos oficios deben desarrollarse con tal agilidad que permita la producción masiva de las prendas de vestir dentro de una fábrica, y para responder a ellos varias empresas han logrado el desarrollo de software permitiendo generar mayor rendimiento, este es el caso por ejemplo de Audaces 360, que ofrece un paquete completo para suplir este tipo de necesidades. 
+      .col-lg-4
+        figure
+          img(src='@/assets/curso/tema1/img01.svg')
+      .col-lg-1
+
+    .row.mb-5
+      .col-lg-1
+      .col-lg-10
+        .cajon.color-acento-contenido.fondo-rojo-claro-1.py-5  
+          .row.mb-4
+            .col-lg-1
+            .col-lg-10
+              p Audaces 360 es un conjunto de software que integra las etapas creativa y productiva para desarrollar la planeación de una empresa o fabrica dentro de sus diferentes líneas, estos softwares son:
+            .col-lg-1     
+          .row.px-5       
+            .col-lg-5
+              figure.mb-4
+                img(src='@/assets/curso/tema1/img02.svg')
+            .col-lg-7.d-flex.flex-column    
+              div(style="height: 17%")
+                p <b>Audaces Idea:</b> para la creación de fichas técnicas y geometrales digitales.
+              div(style="height: 17%")
+                p <b>Audaces 4D:</b> desarrollo de elementos, prendas en 4D.
+              div(style="height: 17%")
+                p <b>Audaces ISA:</b> gestión de colecciones, programador.
+              div(style="height: 17%")
+                p <b>Audaces Patrones:</b> desarrollo de patronaje y escalado de prendas.
+              div(style="height: 17%")
+                p <b>Audaces Tizada:</b> desarrollo de trazo de moldes a partir del patronaje de prendas.
+              div(style="height: 16%")
+                p <b>Audaces Supera:</b> servidor velos de tizadas.
+      .col-lg-1
+
+    p.mb-5 Como se puede observar, esta línea de software suple cada una de las áreas dentro de los departamentos productivos integrados en el departamento de la generación de modelos de prendas de vestir, a continuación, se describirán las características generales del software enfocado en el desarrollo de patrones y escalado de prendas de vestir. 
+
+    h3 Características Audaces Patrones
+    .row.fondo-gris-1.py-5.mb-5
+      .col-lg-1
+      .col-lg-10.py-3
+        figure.mb-4
+          p.BorTit <b>Figura 1.</b> Interfaz general de Audaces Patrones
+          img(src='@/assets/curso/tema1/img03.png')
+          figcaption Fuente: SENA 2021
+      .col-lg-1
+
+    .row.mb-5
+      .col-lg-1
+      .col-lg-10
+        .cajon.color-acento-contenido.fondo-rojo-claro-1.py-5  
+          .row
+            .col-lg-1
+            .col-lg-10.py-4
+              p.mb-5 Audaces Patrones permite construir de manera dinámica patrones con alta precisión para ello emplea una ubicación espacial basada en el plano cartesiano y ángulos que permiten ser muy exactos en el momento de generar cualquier tipo de patrón, adicionalmente permite construir patrones con nivel avanzado como ecualizaciones y amplitudes, entre otros.
+              p Su interfaz es muy intuitiva pues está diseñado para que el programa en sí mismo, le indique al usuario el paso a seguir en cada herramienta que se vaya a implementar, este software escala y modifica patrones ya creados.
+            .col-lg-1
+      .col-lg-1
+      
+    p.mb-5 <b>Dentro de sus características generales:</b>
+
+    .row.mb-5
+      .col-lg-1
+      .col-lg-10
+        ol.lista-ol--cuadro.lista-ol--separador
+          li
+            .lista-ol--cuadro__vineta.color-acento-botones
+              span a
+            | No requiere de programación especial para disponer los patrones para la tizada (trazo).          
+          li
+            .lista-ol--cuadro__vineta.color-acento-botones
+              span b
+            | Se pueden copiar moldes o patrones de un archivo a otro sin necesidad de generar un nuevo archivo.
+          li
+            .lista-ol--cuadro__vineta.color-acento-botones
+              span c
+            | Cuenta con funciones de guardado en versiones anteriores y se pueden abrir sin ningún inconveniente.
+          li 
+            .lista-ol--cuadro__vineta.color-acento-botones
+              span d
+            | Contiene herramientas que propician la construcción de patrones desde cero, transformación, marcaciones, mediciones, escalado y posee características que permiten el complemento con los demás paquetes de softwares.
+          li 
+            .lista-ol--cuadro__vineta.color-acento-botones
+              span e
+            | Interfaz intuitiva y muy fácil de manejar.
+          li 
+            .lista-ol--cuadro__vineta.color-acento-botones
+              span f
+            | Se pueden imprimir moldes o patrones directamente a una impresora a la escala que se desee. 
+          li 
+            .lista-ol--cuadro__vineta.color-acento-botones
+              span g
+            | No hay límite de espacio, es demasiado amplio para poder integrar en un mismo archivo muchos patrones. 
+          li 
+            .lista-ol--cuadro__vineta.color-acento-botones
+              span h
+            | Permite la marcación de cada uno de los nombres de los patrones, descripciones o acotaciones especiales que se necesiten para el ploteo de los moldes.
+          li 
+            .lista-ol--cuadro__vineta.color-acento-botones
+              span i
+            | Cuenta con herramientas de texto.
+          li 
+            .lista-ol--cuadro__vineta.color-acento-botones
+              span j
+            | Cuenta con sistema de contraseñas para archivos determinados, favoreciendo la confidencialidad. Esto sucede dado que en varias empresas manejan básicos estrella, en los que no todo el personal está autorizado a entrar y trabajar sobre ellos, solo aquel personal autorizado por la empresa. 
+          li 
+            .lista-ol--cuadro__vineta.color-acento-botones
+              span k
+            p <b>El programa que complementa a Audaces Patrones es Audaces Tizada</b>, en donde una vez se terminan de desarrollar el respectivo patronaje y escalado se pasa al trazado de los moldes en un espacio que está determinado a partir de anchos de tela y largos de tendido para producción, lo cual posteriormente pasara a ser el plano que guie el tendido y corte de la producción a confeccionar.
+
+    .row.fondo-gris-1.py-5.mb-5
+      .col-lg-1
+      .col-lg-10.py-3
+        figure.mb-4
+          p.BorTit <b>Figura 2.</b> Interfaz Audaces Tizada
+          img(src='@/assets/curso/tema1/img04.png')
+          figcaption Fuente: SENA 2021
+      .col-lg-1
+
+    .row.mb-5
+      .col-lg-2
+      .col-lg-8.px-3
+        a.anexo(href="https://www.youtube.com/watch?v=C0O6derJC2s" target="_blank")
+          .anexo__icono
+            img(src="@/assets/template/icono-link.svg")
+          .anexo__texto
+            p <b>Audaces Tizada.</b> A continuación, le invitamos a visualizar el video de “Herramientas digitales moda”, en donde se podrá apreciar el paso a paso para la elaboración de un patrón en el Software.
+      .col-lg-2
+
+    Separador
+
+    #t_1_1.titulo-segundo
+      h2 1.1  Funciones básicas
+
+    p.mb-4 Este programa cuenta con una serie de pestañas que tiene integrado de manera secuencial, sin embargo, las vistas se pueden configurar y ajustar según lo considere el patronista, a continuación, se muestran cada una de las pestañas:
+
+    SlyderCTitulo.mb-5(:datos="datosSlyder")
+
+    p Para profundizar en la comprensión del funcionamiento de las herramientas básicas del programa, se invita a visualizar los siguientes videos:
+
+    .row.mb-5
+      .col-lg-2
+      .col-lg-8
+        .row.mb-4
+          .col-lg-1
+          .col-lg-5
+            p.mb-4 <b> 1. Interfaz de Audaces</b>
+            a.anexo(href="https://www.youtube.com/watch?v=lSZta00zN0A" target="_blank")
+              .anexo__icono
+                img(src="@/assets/template/icono-link.svg")
+              .anexo__texto
+                p <b>Enlace web.</b> Interfaz de Audaces
+          .col-lg-5
+            p.mb-4 <b>3. Elemento vs Patrón</b>
+            a.anexo(href="https://www.youtube.com/watch?v=2TVCOrR_ECA" target="_blank")
+              .anexo__icono
+                img(src="@/assets/template/icono-link.svg")
+              .anexo__texto
+                p <b>Enlace web.</b> Elemento vs Patrón
+          .col-lg-1
+        .row
+          .col-lg-1
+          .col-lg-5
+            p.mb-4 <b> 2. Elementos básicos de Audaces</b>
+            a.anexo(href="https://www.youtube.com/watch?v=X4KJlTEPamA" target="_blank")
+              .anexo__icono
+                img(src="@/assets/template/icono-link.svg")
+              .anexo__texto
+                p <b>Enlace web.</b> Elementos básicos de Audaces
+          .col-lg-5
+            p.mb-4 <b>4. Snap clásico automático</b>
+            a.anexo(href="https://www.youtube.com/watch?v=UMZiH0aZQNo" target="_blank")
+              .anexo__icono
+                img(src="@/assets/template/icono-link.svg")
+              .anexo__texto
+                p <b>Enlace web.</b> Snap clásico automático
+          .col-lg-1
+      .col-lg-2
+
+    Separador
+
+    #t_1_2.titulo-segundo
+      h2 1.2 Patronaje en Audaces de prendas inferiores y superiores
+
+    .row.mb-5
+      .col-lg-6
+        div.fondo-a.p-5(style="height:100%")
+          p.mb-5(style="height:8%") Para observar cómo se patrona en el software Audaces patrones es importante reconocer como se ve un patrón.
+          figure
+            p.BorTit <b>Figura 2.</b> Interfaz Audaces Tizada
+            img(src='@/assets/curso/tema1/img13.png')
+            figcaption Fuente: SENA 2021
+      .col-lg-6
+        div.fondo-b.p-5(style="height:100%")
+          p.mb-5(style="height:8%") A continuación, se observa un patrón escalado con costuras
+          figure
+            p.BorTit <b>Figura 2.</b> Interfaz Audaces Tizada
+            img(src='@/assets/curso/tema1/img14.png')
+            figcaption Fuente: SENA 2021
+
+    p.mb-5 Para observar cómo funciona el programa ya entrado en práctica se invita al aprendiz visualizar el siguiente video sobre la creación de una prenda inferior en el software de Audaces Patrones.
+    
+    .row.mb-5
+      .col-lg-1
+      .col-lg-10
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    .col-lg-1
+
+    p.mb-5 Así mismo, se invita al aprendiz a profundizar en la creación e interpretación de una prenda superior en el software de Audaces Patrones, teniendo en cuenta que ya se pudo ver como se construía un patrón desde cero, en el video se podrá observar un ejercicio de interpretación.
+
+     .row.mb-5
+      .col-lg-1
+      .col-lg-10
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      .col-lg-1
+
+    Separador
+
+    #t_1_3.titulo-segundo
+      h2 1.3 Escalado de patrones en “Audaces Patrones”
+
+    p.mb-5 Como se ha mencionado para dar una correcta aplicación a las herramientas de escalado se debe tener presente que se aplicarán las medidas o datos numéricos acordes a la metodología del patronista y/o escalador, para ello en el programa se debe establecer las tallas primero a partir del patrón base, como se muestra en la imagen siguiente, una vez creado el patrón, se le da clic derecho y se selecciona propiedades, seguido, se pincha en la pestaña de escalado y se asignan las respectivas tallas.
+    
+    .row.mb-5.py-5.fondo-a
+      .col-lg-1
+      .col-lg-10.py-3
+        figure
+          p.BorTit <b>Figura 13.</b> Propiedades, escalado para asignación de tallas.
+          img(src='@/assets/curso/tema1/img15.png')
+          figcaption Fuente: SENA 2021
+      .col-lg-1
+
+    p.mb-5 En la figura anterior se observa que la talla base, está señalada por el programa en rojo y las demás no, a estas se les asigna colores diferentes, pinchando en cada uno de los cajones, esto para cuando se esté escalando se puedan diferenciar las líneas de los patrones resultantes. 
+
+    .row.mb-5.py-5.fondo-gris-1
+      .col-lg-1
+      .col-lg-10.py-3
+        figure
+          p.BorTit <b>Figura 14.</b> Escalado por tablas
+          img(src='@/assets/curso/tema1/img16.png')
+          figcaption Fuente: SENA 2021
+      .col-lg-1
+
+    p.mb-5 En la figura anterior se observa que una vez se pincha en tabla dentro de la pestaña de escalado se abre una ventana en la que en cada casilla se asignaran lo valores a escalar, también se observa otros botones en la parte inferior que sirve para agilizar este proceso, para comprender más sobre el escalado, se invita a visualizar el video: 
+    
+    .row.mb-5
+      .col-lg-1
+      .col-lg-10
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      .col-lg-1
+    //.align-items-center
+    //.justify-content-center
+</template>
+
+<script>
+import SlyderCTitulo from '../components/SlyderCTitulo'
+export default {
+  name: 'Tema1',
+  components: {
+    SlyderCTitulo,
+  },
+  data: () => ({
+    datosSlyder: [
+      {
+        figura: 'Figura 3. ',
+        tituloImagen: 'Pestaña de inicio',
+        imagen: require('@/assets/curso/tema1/img05.png'),
+        titulo: 'A. Herramientas de inicio',
+        descripcion:
+          'Contiene la configuración del color de la línea, ancho de líneas, opciones para copiar, pegar, borrar, seleccionar y restaurar los cambios.',
+      },
+      {
+        figura: 'Figura 4. ',
+        tituloImagen: 'Herramientas de construcción',
+        imagen: require('@/assets/curso/tema1/img06.png'),
+        titulo: 'B. Herramientas de construcción',
+        descripcion:
+          'Contiene opciones para realizar rectas, rectángulos, cuadrados, prolongación de rectas y las relacionadas a ángulos, curvas, editor de curvas, configuración de puntos, puntos por coordenadas y circunferencias por aplicación de formula y ángulos, estás basadas en operaciones básicas de geometría y finaliza con textos.  De aquí parte toda la construcción inicial de un básico de patrón.',
+      },
+      {
+        figura: 'Figura 5. ',
+        tituloImagen: 'Pestaña de producción',
+        imagen: require('@/assets/curso/tema1/img07.png'),
+        titulo: 'C. Herramientas de producción',
+        descripcion:
+          'Contiene las opciones para crear patrones, una vez se han construido los planos, estos deben crearse. También contiene la opción de digitalización de moldes, logrado a partir de mesa digitalizadora (por ejemplo), que consiste en ubicar moldes reales en la mesa y esta los pasa al software, así como las opciones de costura, dobladillos, doblar, desdoblar, pliegues, pinzas, cortar moldes, unir moldes o cortar y unir.',
+      },
+      {
+        figura: 'Figura 6. ',
+        tituloImagen: 'Pestaña de marcaciones',
+        imagen: require('@/assets/curso/tema1/img08.png'),
+        titulo: 'D. Herramientas de marcaciones',
+        descripcion:
+          'Contiene las opciones de marcaciones por piquetes, así como la redistribución y borrar los mismos, alineación, sustitución y modificación de piquetes, también contiene la opción de introducir o borrar elementos, útil cuando se debe marcar una línea de corte dentro de un patrón, como en las mangas largas con abertura para sesgo. ',
+      },
+      {
+        figura: 'Figura 7. ',
+        tituloImagen: 'Pestañas de escalado',
+        imagen: require('@/assets/curso/tema1/img09.png'),
+        titulo: 'E. Herramientas de escalado',
+        descripcion:
+          'Contiene las opciones para escalar los patrones, herramienta sumamente importante, una vez se tengan los patrones debidamente marcados y con asignación de tallas, se procede a escalar implementando los datos numéricos para incrementar o reducir un patrón, es de aclarar que el escalador y patronista deben tener muy claro dichos valores y metodología a implementar en el patrón.',
+      },
+      {
+        figura: 'Figura 8. ',
+        tituloImagen: ' Pestaña de manipulación',
+        imagen: require('@/assets/curso/tema1/img10.png'),
+        titulo: 'F. Herramientas de manipulación',
+        descripcion:
+          'Contiene las opciones de modificación de puntos dentro de los patrones, reflejo, paralelas, divisiones de segmentos, rotaciones, giros, proporciones, así como transformar rectas a curvas de un patrón, introducir y borrar puntos de control (indispensables para escalado), alineación y transformación de líneas. ',
+      },
+      {
+        figura: 'Figura 9. ',
+        tituloImagen: 'Pestaña de conferencia',
+        imagen: require('@/assets/curso/tema1/img11.png'),
+        titulo: 'G. Herramientas de conferencia',
+        descripcion:
+          'Contiene opciones de comparar medidas entre patrones, medición (cotas) de perímetros, distancias, ángulos, radios y otros complementos que permite comparar perímetros y hallar medidas por elementos en coordenadas. ',
+      },
+      {
+        figura: 'Figura 10. ',
+        tituloImagen: 'Pestaña de modelo',
+        imagen: require('@/assets/curso/tema1/img12.png'),
+        titulo: 'H. Herramientas de modelo',
+        descripcion:
+          'Se considera modelo a los patrones desarrollados en un mismo archivo con las mismas características en cuanto a numeración de tallas y propiedades de los patrones, en este apartado se pueden observar las propiedades generales, información, autor, telas, variantes, relación de piquetes, imágenes o también se pueden buscar modelos. ',
+      },
+    ],
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass" scoped></style>
